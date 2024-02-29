341e8af15ca95e7da70994cd3878aa0f9f4339db
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-02-29 10:04:42 -0500

    Update raytracer.rs

--- src/raytracer.rs
+++ src/raytracer.rs
@@ -53,6 +53,10 @@ fn start(mut commands: Commands, asset_server: Res<AssetServer>, mut window: Que
       TextureFormat::Rgba8UnormSrgb,
       RenderAssetUsages::all(),
     )),
+    sprite: Sprite {
+      flip_y: true,
+      ..Default::default()
+    },
     ..Default::default()
   }));
 }
@@ -87,44 +91,61 @@ fn resize(
 fn render(
   mut query: Query<&mut Handle<Image>, With<RenderTexture>>,
   mut images: ResMut<Assets<Image>>,
-  mut image_settings: ResMut<RenderSettings>,
+  mut _image_settings: ResMut<RenderSettings>,
 ) {
   query.iter_mut().for_each(|image| {
     let Some(image) = images.get_mut(image.id()) else {
       return;
     };
 
-    let width = image.width();
-    let height = image.height();
-    let mut i = 0;
-    let mut iter = image.data.iter_mut();
-    while let Some(pixel_tuple) = iter.next_tuple::<(&mut u8, &mut u8, &mut u8, &mut u8)>() {
-      let x = i as f32 / width as f32;
-      let y = i as f32 % height as f32;
+    let UVec2 { x: width, y: height } = image.size();
+    let aspect_ratio = width as f32 / height as f32;
 
-      let color = pixel([x, y]);
+    let mut iterator = image.data.iter_mut();
+    'outer: for y in 0..height {
+      for x in 0..width {
+        let mut coord = [x as f32 / width as f32, y as f32 / height as f32];
+        coord = [coord[0] * 2.0 - 1.0, coord[1] * 2.0 - 1.0];
+        coord = [coord[0] * aspect_ratio, coord[1]];
+        let Some(next) = iterator.next_tuple::<(&mut u8, &mut u8, &mut u8, &mut u8)>() else {
+          break 'outer;
+        };
 
-      *pixel_tuple.0 = color[0];
-      *pixel_tuple.1 = color[1];
-      *pixel_tuple.2 = color[2];
-      *pixel_tuple.3 = color[3];
+        let color = pixel(coord);
 
-      i += 1;
+        *next.0 = color[0];
+        *next.1 = color[1];
+        *next.2 = color[2];
+        *next.3 = color[3];
+      }
     }
   });
 }
 
 fn pixel(frag_pos: [f32; 2]) -> [u8; 4] {
-  let mut color = [255; 4];
+  let mut color = [0, 0, 0, 255];
 
-  color[0] = fastrand::u8(0..=255);
-  color[1] = fastrand::u8(0..=255);
-  color[2] = fastrand::u8(0..=255);
+  let ray_origin = Vec3::new(0.0, 0.0, 2.0);
+  let ray_dir = Vec3::new(frag_pos[0], frag_pos[1], -1.0).normalize();
+  let radius = 0.5;
 
-  color
+  color[0] = (ray_dir.x * 255.0) as u8;
+  color[1] = (ray_dir.y * 255.0) as u8;
+
+  let a = ray_dir.dot(ray_dir);
+  let b = 2.0 * ray_origin.dot(ray_dir);
+  let c = ray_origin.dot(ray_origin) - radius * radius;
+
+  let discriminant = b * b - 4.0 * a * c;
+
+  if discriminant >= 0.0 {
+    [255, 255, 255, 255]
+  } else {
+    [0, 0, 0, 255]
+  }
 }
 
-fn ui(mut contexts: EguiContexts, mut image_settings: ResMut<RenderSettings>) {
+fn ui(mut contexts: EguiContexts, mut _image_settings: ResMut<RenderSettings>) {
   egui::Window::new("Settings")
     .anchor(Align2::LEFT_BOTTOM, (10., -10.))
     .resizable(false)
